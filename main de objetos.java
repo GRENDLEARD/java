399f6b885472538914b8ba3e91080cc0174014ef
GRENDLEARD <134238909+GRENDLEARD@users.noreply.github.com>
2023-09-27 13:13:43 -0500

    Create main de objetos.java

---
+++ main de objetos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquetito;
+
+/**
+ *
+ * @author SALAS
+ */
+
+public class Main {
+   
+   public static void main(String [] args){
+       
+       Persona p1 = new Persona("Santiago",40,"Calle 54b Sur#89a 11","Desarrollo de sofware","2838843773");
+       
+       p1.MostrarDatos();
+     
+       
+       
+   }
+    
+  
+    
+}
