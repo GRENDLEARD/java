815fa7d1492f359ec04bba5a390e38789d43a649
GRENDLEARD <134238909+GRENDLEARD@users.noreply.github.com>
2023-10-17 14:55:18 -0500

    Actualizar main.java

--- main.java
+++ main.java
@@ -1,37 +1,41 @@
-package paquetito;
+public class CompaniaTelefonia {
+    private String nombre;
+    private int minutosIncluidos;
+    private int internetIncluido;
+    private double costoMensual;
 
-public class Main {
+    public CompaniaTelefonia(String nombre, int minutosIncluidos, int internetIncluido, double costoMensual) {
+        this.nombre = nombre;
+        this.minutosIncluidos = minutosIncluidos;
+        this.internetIncluido = internetIncluido;
+        this.costoMensual = costoMensual;
+    }
 
-    public static void main(String[] args) {
-        Persona persona1 = new Persona("David", 18, "12345678", "Hombre", 70, 190);
-        Persona persona2 = new Persona("Maria", 25, "105891209", "Mujer", 60, 160);
-     
-        
-        
-        System.out.println("Persona 1:");
-        System.out.println(persona1.toString());
-        int imc1 = persona1.calcularIMC();
-        System.out.println("IMC: " + imc1);
-        String estadoIMC1;
-        estadoIMC1 = switch (imc1) {
-            case -1 -> "Peso por debajo del ideal";
-            case 0 -> "Peso ideal";
-            default -> "Sobrepeso";
-        };
-        System.out.println("Estado IMC: " + estadoIMC1);
-        System.out.println("Mayor de edad: " + persona1.esMayorDeEdad());
+    public String getNombre() {
+        return nombre;
+    }
 
-        System.out.println("\nPersona 2:");
-        System.out.println(persona2.toString());
-        int imc2 = persona2.calcularIMC();
-        System.out.println("IMC: " + imc2);
-        String estadoIMC2;
-        estadoIMC2 = switch (imc2) {
-            case -1 -> "Peso por debajo del ideal";
-            case 0 -> "Peso ideal";
-            default -> "Sobrepeso";
-        };
-        System.out.println("Estado IMC: " + estadoIMC2);
-        System.out.println("Mayor de edad: " + persona2.esMayorDeEdad());
+    public double calcularPuntaje(int minutos, int internet, double costoMaximo) {
+        double puntaje = 0.0;
+
+        if (minutos <= minutosIncluidos) {
+            puntaje += 1.0;
+        } else {
+            puntaje += (double) minutosIncluidos / minutos;
+        }
+
+        if (internet <= internetIncluido) {
+            puntaje += 1.0;
+        } else {
+            puntaje += (double) internetIncluido / internet;
+        }
+
+        if (costoMensual <= costoMaximo) {
+            puntaje += 1.0;
+        } else {
+            puntaje += costoMaximo / costoMensual;
+        }
+
+        return puntaje;
     }
 }
